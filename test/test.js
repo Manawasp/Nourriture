7995e3d6d5ad88d05aaed055d23927bec23e3afd
Kyndt Clovis <clovis.kyndt@epitech.eu>
2014-11-16 11:35:00 +0800

    prepare merge

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var request = request('supertest');
-var app = require('../app.js');
-
-describ('GET /',function(){
-	it('response with Hello world',function(done){
-		request(app).get('/').expect('hello world',done);
-	});
-});
