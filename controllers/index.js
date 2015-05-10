9c88b42f812401fe0c8b0c32121c75a5f13f455e
Kyndt Clovis <clovis.kyndt@epitech.eu>
2015-05-10 18:44:17 +0800

    catch page not found and send json error

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 var express = require('express')
   , router = express.Router();
 
+/**
+ * Aplication route
+ */
+
 router.use('/users',        	require('./users'));
 router.use('/followers',    	require('./followers'));
 router.use('/followeds',    	require('./followeds'));
@@ -14,5 +18,23 @@ router.use('/like/recipes',   	require('./recipes_like'));
 router.use('/like/moments',    	require('./moments_like'));
 router.use('/pictures/',   		require('./pictures'));
 router.use('/homepage/',   		require('./comrecipe'));
+
+/**
+ * 404 no route catching
+ */
+
+notfound = function(req, res){
+  res.send(404, {error: "route not found"});
+}
+
+router.post('*', notfound);
+router.get('*', notfound);
+router.put('*', notfound);
+router.patch('*', notfound);
+router.delete('*', notfound);
+
+/**
+ * Export module
+ */
 
 module.exports = router
