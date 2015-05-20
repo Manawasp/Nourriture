39d78a9c5050a4161cd7e7cd49c2a8902b1091f3
Kyndt Clovis <clovis.kyndt@epitech.eu>
2015-05-20 16:50:45 +0800

    reactivate log + update dockercompose

--- controllers/services/log.js
+++ controllers/services/log.js
@@ -3,24 +3,24 @@
  * Static var
  */
 
-// var	bunyan      = require('bunyan');
-	// , log         = bunyan.createLogger({
-	// 									name: "foodapi",
-	// 									streams: [{
-	// 										path: 'api.log'
-	// 									}]
-	// 								});
+var	bunyan      = require('bunyan')
+	, log         = bunyan.createLogger({
+										name: "foodapi",
+										streams: [{
+											path: 'api.log'
+										}]
+									});
 
 exports.writeLog = function(req, resource, status, resBody) {
-	// log.info(	{"method": 		req.method,
-	// 					"ressource": 	resource,
-	// 					"status":			status,
-	// 					"req": {
-	// 						"path": req.path,
-	// 						"body": req.body
-	// 					},
-	// 					"res": {
-	// 						"body": resBody
-	// 					}
-	// 				})
+	log.info(	{"method": 		req.method,
+						"ressource": 	resource,
+						"status":			status,
+						"req": {
+							"path": req.path,
+							"body": req.body
+						},
+						"res": {
+							"body": resBody
+						}
+					})
 }
